215a2f6d6850467d39e3419978e9acb5f3900133
Vanfarock <vinicius.m.m.2002@gmail.com>
2021-04-20 21:12:00 -0300

    Hiding sentry private key

--- vidly/src/services/logService.js
+++ vidly/src/services/logService.js
@@ -1,10 +1,10 @@
 import * as Sentry from "@sentry/react";
+import config from "../config.json";
 import { Integrations } from "@sentry/tracing";
 
 function init() {
   Sentry.init({
-    dsn:
-      "https://806123b4327842a8b7f27efc51a2f70e@o575001.ingest.sentry.io/5726717",
+    dsn: `https://${config.sentryPrivateKey}@o575001.ingest.sentry.io/5726717`,
     integrations: [new Integrations.BrowserTracing()],
     tracesSampleRate: 1.0,
   });
